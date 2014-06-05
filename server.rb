0654effa610385551dbfb65791ae32c9c43dab95
c13iscuit <barnett.chri@gmail.com>
2014-06-05 12:37:31 -0400

    Completed logic for reading and displaying accounts and transactions

--- server.rb
+++ server.rb
@@ -1,25 +1,37 @@
 require 'sinatra'
 require 'csv'
+require 'pry'
 
-file = 'bank_data.csv'
+trans_file = 'bank_data.csv'
+accounts_file = 'balances.csv'
 
-def build_data(file)
+def build_transactions(trans_file)
   @transactions = []
-  CSV.foreach(file, headers: true) do |row|
-    trans_hash = {}
-    trans_hash[:date] = row["Date"]
-    trans_hash[:amount] = row["Amount"]
-    trans_hash[:description] = row["Description"]
-    trans_hash[:account] = row["Account"]
-    @transactions << trans_hash
+  CSV.foreach(trans_file, headers: true) do |row|
+    transaction = BankTransaction.new(row["Date"], row["Amount"], row["Description"], row["Account"])
+    @transactions << transaction
   end
-  puts @transactions
+end
+
+def build_accounts(accounts_file)
+  @accounts = []
+  CSV.foreach(accounts_file, headers: true) do |row|
+    account = BankAccount.new(row["Account"], row["Balance"])
+    @accounts << account
+  end
 end
 
 class BankTransaction
+  attr_reader :date, :amount, :description, :account
+  def initialize(date, amount, description, account)
+    @date = date
+    @amount = amount
+    @description = description
+    @account = account
+  end
 
   def deposit?(amount)
-    if amount.to_i > 0
+    if @amount.to_i > 0
       true
     else
       false
@@ -27,29 +39,49 @@ class BankTransaction
   end
 
   def summary
-    #return a string of the transaction
+    @string = "#{@date}: #{@account} #{@amount}, #{@description}"
+    @string
   end
-
 end
 
 class BankAccount
+  attr_reader :account, :balance
+  def initialize(account, balance)
+    @account = account
+    @balance = balance
+  end
 
   def starting_balance
-    #that returns the starting balance loaded from the file
+    @balance
   end
 
   def current_balance
-    #that returns the ending balance after all transactions have been processed
+    @amount_trans = []
+    build_transactions('bank_data.csv')
+    @transactions.each do |transaction|
+      if transaction.account == account
+        @amount_trans << transaction.amount.to_f
+      end
+    end
+    total = @amount_trans.inject(:+)
+    @new_balance = @balance.to_f + total.to_f
+    @new_balance
   end
 
-  def summary
-    #that returns an array of all of the transaction summaries
+  def summary(account)
+    @account_trans = []
+    build_transactions('bank_data.csv')
+    @transactions.each do |transaction|
+      if transaction.account == account
+        @account_trans << transaction.summary
+      end
+    end
+    @account_trans
   end
-
 end
 
 get '/' do
-  build_data(file)
+  build_transactions(trans_file)
+  build_accounts(accounts_file)
   erb :index
 end
-
